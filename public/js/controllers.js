aa6cfebea959c27ce59a8adba4b12c52e84c72ed
Jackson Foley <jacksoncfoley@gmail.com>
2014-01-06 00:27:20 -0500

    fcked w/ controllers

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -8,14 +8,18 @@ angular.module('graphTherapyApp.controllers', []).
 
 	    $scope.tweets = [];
 
-	    $scope.loadTweets = function() {
-	        var httpRequest = $http({
+			$scope.loadTweets = function() {
+			//var httpRequest   = 
+			$http({
 	            method: 'GET',
-	            url: '/api/getTweets'
+	            url: '/api/getlast/10'
 
-	        }).success(function(data, status) {
+	        }).success(function (data, status) {
 	            $scope.tweets = data;
-	        });
+	            console.log(data);
+	        }).error(function (data, status, headers, config) {
+				console.log('error');
+			});;
 	    };
 	}).
 	controller('graph_A_Ctrl', function ($scope, $http) {
