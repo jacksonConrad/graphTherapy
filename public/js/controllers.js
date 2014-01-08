87d3ca6abc435b6b3557a54b28b50db17bcf7802
lukeguild <luke.guild@gmail.com>
2014-01-07 19:58:06 -0500

    change to controllers.js GET function

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,10 +3,10 @@
 
 /* Controllers */
 
-angular.module('graphTherapyApp.controllers', []).
+angular.module('graphTherapyApp.controllers', []);
 	controller('tweetsCtrl', function ($scope, $http, $rootScope, $location) {
 
-	    $scope.tweets = [];
+	    /*$scope.tweets = [];
 
 			$scope.loadTweets = function() {
 			//var httpRequest   = 
@@ -14,14 +14,27 @@ angular.module('graphTherapyApp.controllers', []).
 	            method: 'GET',
 	            url: '/api/getlast/10'
 
-	        }).success(function (data, status) {
+	        }).success(function (data, status, headers, config) {
 	            $scope.tweets = data;
 	            console.log(data);
 	        }).error(function (data, status, headers, config) {
 				console.log('error');
 			});;
-	    };
-	}).
+	    };*/
+
+	    //new method of grabbing - the get looks different but it's just a shorthand of the same thing
+	    graphTherapyApp.loadTweets('tweetsGet', function($http) {
+
+	    	var tweets  = {content:null};
+
+	    	$http.get('content.json').success(function(data) {
+
+	    		tweets.content = data;
+	    	});
+
+	    	return tweets;
+	    });
+	});
 	controller('graph_A_Ctrl', function ($scope, $http) {
 	// write Ctrl here
 
